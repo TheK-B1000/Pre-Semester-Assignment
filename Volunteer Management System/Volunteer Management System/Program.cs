5272a932cc33834e8b29e8e816ab75a3443e635b
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-07-26 16:17:12 -0400

    Added Views

--- Volunteer Management System/Volunteer Management System/Program.cs
+++ Volunteer Management System/Volunteer Management System/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello There!");
+
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
