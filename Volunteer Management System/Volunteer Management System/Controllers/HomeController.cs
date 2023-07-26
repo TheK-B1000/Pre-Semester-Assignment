40edace0d7b61520ecb00291a0ef1c0ac7de165d
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-07-26 10:40:39 -0400

    01 - Initial Commit

---
+++ Volunteer Management System/Volunteer Management System/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Volunteer_Management_System.Controllers
+{
+     public class HomeController : Controller
+     {
+        
+         //constructor
+         public HomeController()
+         {
+
+         }
+
+         public IActionResult Index()
+         {
+            return View();
+         }
+     }
+}
