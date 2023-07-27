2f82d631419daa60170b27c7245749b85bb873e7
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-07-27 16:33:59 -0400

    Create NavigationMenuViewComponent

---
+++ Volunteer Management System/Volunteer Management System/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Volunteer_Management_System.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
