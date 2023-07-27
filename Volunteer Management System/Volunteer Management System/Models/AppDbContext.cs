3f19687094df110e107abe28082dd4637488be92
bf895677 <fernanda.delcatillo.14@gmail.com>
2023-07-27 15:25:12 -0400

    created EFPersonRepository

--- Volunteer Management System/Volunteer Management System/Models/AppDbContext.cs
+++ Volunteer Management System/Volunteer Management System/Models/AppDbContext.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using Volunteer_Management_System.Models;
 //using System.Collections.Generic;
 
-namespace VolunteerManagementSystem.Models
+namespace Volunteer_Management_System.Models
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         { }
 
-        public DbSet<Volunteer> Volunteer { get; set; }
-        public DbSet<Person> Person { get; set; }
-        public DbSet<Opportunity> Opportunity { get; set; }
-        public DbSet<Admin> Admin { get; set; }
+        public DbSet<Volunteer> Volunteers { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Opportunity> Opportunities { get; set; }
+        public DbSet<Admin> Admins { get; set; }
     }
 }
---
+++ Volunteer Management System/Volunteer Management System/Models/EFPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using Volunteer_Management_System.Models;
+
+namespace Volunteer_Management_System.Models
+{
+    
+     public class EFPersonRepository : IPersonRepository
+      {
+          private AppDbContext context;
+          public EFPersonRepository(AppDbContext ctx)
+          {
+              context = ctx;
+          }
+        public IEnumerable<Person> People => context.People;
+       }
+    
+}
