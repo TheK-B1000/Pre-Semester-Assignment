bef1db37d2ae24bc6b807024683433161a83eddb
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-07-27 12:49:52 -0400

    Update Program.cs

--- Volunteer Management System/Volunteer Management System/Program.cs
+++ Volunteer Management System/Volunteer Management System/Program.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 
+// Add services to the container
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello There!");
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapGet("/hi", () => "Hello There!");
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 app.UseStaticFiles();
 app.UseRouting();
