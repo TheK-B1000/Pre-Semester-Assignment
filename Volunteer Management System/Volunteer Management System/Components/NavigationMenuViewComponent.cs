bee4100628d936ffe4465e7e72eee2e62dabf9d0
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-07-27 16:58:59 -0400

    Adding the Categories List

--- Volunteer Management System/Volunteer Management System/Components/NavigationMenuViewComponent.cs
+++ Volunteer Management System/Volunteer Management System/Components/NavigationMenuViewComponent.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Volunteer_Management_System.Models;
 
 namespace Volunteer_Management_System.Components
 {
     public class NavigationMenuViewComponent : ViewComponent
     {
-        public string Invoke()
+        private IPersonRepository repository;
+        public NavigationMenuViewComponent(IPersonRepository repo)
         {
-            return "Hello from the Nav View Component";
+            repository = repo;
         }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["approval"];
+            return View(repository.People
+            .Select(x => x.Approval)
+            .Distinct()
+            .OrderBy(x => x));
+        }
+
     }
 }
