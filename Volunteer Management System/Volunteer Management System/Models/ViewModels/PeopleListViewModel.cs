5dabd59f818dcaedf3eabf9d38bb8c750d4dfa8f
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-07-27 14:05:56 -0400

    Created PeopleListViewModel

---
+++ Volunteer Management System/Volunteer Management System/Models/ViewModels/PeopleListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Volunteer_Management_System.Models;
+
+using Volunteer_Management_System.Models.ViewModels;
+
+namespace Volunteer_Management_System.Models.ViewModels
+{
+    public class PeopleListViewModel
+    {
+        public IEnumerable<Person> People { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
