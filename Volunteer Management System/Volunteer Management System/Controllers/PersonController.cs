149b851e37b7ec1bf75a35ef40a3cb4367dac82f
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-07-27 14:11:58 -0400

    Updated PersonController

--- Volunteer Management System/Volunteer Management System/Controllers/PersonController.cs
+++ Volunteer Management System/Volunteer Management System/Controllers/PersonController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Volunteer_Management_System.Models;
+using Volunteer_Management_System.Models.ViewModels;
 
 namespace Volunteer_Management_System.Controllers
 {
@@ -15,11 +16,25 @@ namespace Volunteer_Management_System.Controllers
         }
 
         private IPersonRepository repository;
+        public int PageSize = 4;
         public PersonController(IPersonRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List() => View(repository.People);
+        public ViewResult List(int page = 1)
+            => View(new PeopleListViewModel
+            {
+                 People = repository.People
+                            .OrderBy(p => p.PersonID)
+                            .Skip((page - 1) * PageSize)
+                            .Take(PageSize),
+                 PagingInfo = new PagingInfo
+                 {
+                     CurrentPage = page,
+                     ItemsPerPage = PageSize,
+                     TotalItems = repository.People.Count()
+                 }
+             });
     }
 }
