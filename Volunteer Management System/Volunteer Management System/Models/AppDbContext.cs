baca62dd9d7c82ceaaf69ea2e24f4fd7e5287cab
bf895677 <fernanda.delcatillo.14@gmail.com>
2023-07-27 14:49:18 -0400

    Create AppDbContext

---
+++ Volunteer Management System/Volunteer Management System/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Volunteer_Management_System.Models;
+//using System.Collections.Generic;
+
+namespace SportsStore.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Volunteer> Volunteer { get; set; }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Opportunity> Opportunity { get; set; }
+        public DbSet<Admin> Admin { get; set; }
+    }
+}
