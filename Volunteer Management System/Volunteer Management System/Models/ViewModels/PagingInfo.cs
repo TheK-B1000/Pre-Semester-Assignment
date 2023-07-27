0d08a67c2d7fef31a03d89fde737c117d150e913
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-07-27 13:59:43 -0400

    Created PagingInfo and PageTagLinkHelper

---
+++ Volunteer Management System/Volunteer Management System/Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Volunteer_Management_System.Models.ViewModels
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+42 
+ public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+    }
+}
