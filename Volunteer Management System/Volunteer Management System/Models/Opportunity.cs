368ca553221311c57f7e5baca1e1ce8c6b82cd82
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-08-02 19:10:06 -0400

    Create Opportunity.cs Model

---
+++ Volunteer Management System/Volunteer Management System/Models/Opportunity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Volunteer_Management_System.Models
+{
+    public class Opportunity
+    {
+        public string OpportunityID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Center { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public TimeSpan Duration { get; set; }
+        [Required]
+        public int MaximumVolunteers { get; set; }
+        public int CurrentVolunteers { get; set; }
+    }
+}
