483bac82cc197658e999b741849c98ef4afddc45
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-08-02 18:37:30 -0400

    Added Vol and Opp Controller

---
+++ Volunteer Management System/Volunteer Management System/Controllers/OpportunityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Volunteer_Management_System.Controllers
+{
+    public class OpportunityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Volunteer Management System/Volunteer Management System/Controllers/VolunteerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Volunteer_Management_System.Controllers
+{
+    public class VolunteerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
