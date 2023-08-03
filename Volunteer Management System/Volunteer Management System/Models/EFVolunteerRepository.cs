8f87373c97b4aeb29a0c70957154a477e9a74f29
bf895677 <fernanda.delcatillo.14@gmail.com>
2023-08-02 20:07:03 -0400

    created efrepository and interface repository for volunteer in the models folder

--- Volunteer Management System/Volunteer Management System/Models/EFVolunteerRepository.cs
+++ Volunteer Management System/Volunteer Management System/Models/EFVolunteerRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace Volunteer_Management_System.Models
+﻿using Volunteer_Management_System.Models;
+
+namespace Volunteer_Management_System.Models
 {
+    
     public class EFVolunteerRepository
     {
+        private AppDbContext context;
+        public EFVolunteerRepository(AppDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IEnumerable<Volunteer> Volunteers => context.Volunteers;
+
+
     }
 }
--- Volunteer Management System/Volunteer Management System/Models/IVolunteerRepository.cs
+++ Volunteer Management System/Volunteer Management System/Models/IVolunteerRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Volunteer_Management_System.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Volunteer_Management_System.Models
 {
     public interface IVolunteerRepository
     {
+        IEnumerable<Volunteer> Volunteers { get; }
     }
 }
