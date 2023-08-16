b9e0264391cab3f19dc79d35cf01ee3a28096ddb
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-08-16 18:40:55 -0400

    Update Manage Volunteers

--- Volunteer Management System/Volunteer Management System/Models/Volunteer.cs
+++ Volunteer Management System/Volunteer Management System/Models/Volunteer.cs
@@ -1,40 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Volunteer_Management_System.Models
 {
-    public class Person
+    public class Volunteer
     {
-        public string PersonID { get; set; }
-        [Required]
+        public string VolunteerID { get; set; }
         public string FirstName { get; set; }
-        [Required]
         public string LastName { get; set; }
-        [Required]
         public string Username { get; set; }
-        [Required]
         public string Password { get; set; }
-        [Required]
         public string Address { get; set; }
-        [Required]
         public string PhoneNumbers { get; set; }
-        [Required]
         public string Email { get; set; }
-        [Required]
-        public string Role { get; set; } // To indicate whether the person is an 'Admin' or 'Volunteer'
-    }
-
-    public class DBAdmin : Person
-    {
-        [Required]
-        public string Title { get; set; }
-    }
-
-    public class DBVolunteer : Person
-    {
+        public string Role { get; set; }
         public string Centers { get; set; }
         public string SkillsOrInterests { get; set; }
         public string AvailabilityTimes { get; set; }
@@ -48,4 +26,3 @@ namespace Volunteer_Management_System.Models
         public string Status { get; set; }
     }
 }
-
