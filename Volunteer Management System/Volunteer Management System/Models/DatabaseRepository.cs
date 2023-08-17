74056ad16c07edf64c66cb8afa4d551f90cb8350
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-08-17 12:35:26 -0400

    Opportunity and Volunteer Matches

--- Volunteer Management System/Volunteer Management System/Models/DatabaseRepository.cs
+++ Volunteer Management System/Volunteer Management System/Models/DatabaseRepository.cs
@@ -138,5 +138,17 @@ namespace Volunteer_Management_System.Models
             return opportunities.Where(o => o.Center == preferredCenter);
         }
 
+        public IEnumerable<Volunteer> GetMatchedVolunteersForOpportunity(string opportunityId)
+        {
+            var opportunity = opportunities.FirstOrDefault(o => o.OpportunityID == opportunityId);
+            if (opportunity == null)
+            {
+                return new List<Volunteer>();
+            }
+
+            var preferredCenter = opportunity.Center;
+            return volunteers.Where(v => v.Centers == preferredCenter);
+        }
+
     }
 }
